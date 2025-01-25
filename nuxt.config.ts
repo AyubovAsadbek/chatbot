c60918b6fcb99b4655b6e6dbc16c927372e82616
AyubovAsadbek <ayubovasadbek33@gmail.com>
2025-01-25 11:00:43 +0500

    refactor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/logo.svg" }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/main-logo.svg" }],
     },
   },
   compatibilityDate: "2024-11-01",
